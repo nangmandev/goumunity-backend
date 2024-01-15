a014b918565843777a262886b7213de8d7bafc25
Ryong <wkdcjdfyd@naver.com>
2024-01-15 16:27:04 +0900

    add: 스프링 초기 폴더 업로드

---
+++ BE/src/main/java/com/ssafy/goumunity/GoumunityApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.goumunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoumunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoumunityApplication.class, args);
+	}
+
+}
---
+++ BE/src/test/java/com/ssafy/goumunity/GoumunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.goumunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoumunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
