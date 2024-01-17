426a944e230ffa0da2934df0a7b082130acbed1a
김규현 <kgh2120@gmail.com>
2024-01-17 16:19:19 +0900

    chore: session redis 설정 추가

--- BE/src/main/java/com/ssafy/goumunity/config/RedisConfig.java
+++ BE/src/main/java/com/ssafy/goumunity/config/RedisConfig.java
@@ -1,5 +1,11 @@
 package com.ssafy.goumunity.config;
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.jsontype.BasicPolymorphicTypeValidator;
+import org.springframework.beans.factory.BeanClassLoaderAware;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,16 +13,20 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.security.jackson2.SecurityJackson2Modules;
 
 @Configuration
-public class RedisConfig {
+public class RedisConfig  implements BeanClassLoaderAware {
     @Value("${spring.redis.host}")
     private String redisHost;
     @Value("${spring.redis.port}")
     private String redisPort;
     @Value("${spring.redis.password}")
     private String redisPassword;
+    private ClassLoader loader;
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
@@ -34,5 +44,24 @@ public class RedisConfig {
         redisTemplate.setKeySerializer(new StringRedisSerializer());
         redisTemplate.setValueSerializer(new StringRedisSerializer());
         return redisTemplate;
+    }
+
+
+    @Bean
+    public RedisSerializer<Object> springSessionDefaultRedisSerializer() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.activateDefaultTyping(BasicPolymorphicTypeValidator.builder()
+                                .allowIfBaseType(Object.class).build(),
+                        ObjectMapper.DefaultTyping.NON_FINAL,
+                        JsonTypeInfo.As.PROPERTY)
+                .registerModules(SecurityJackson2Modules.getModules(loader))
+                .disable(SerializationFeature.WRITE_DATE_KEYS_AS_TIMESTAMPS);
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);;
+        return new GenericJackson2JsonRedisSerializer(objectMapper);
+    }
+
+    @Override
+    public void setBeanClassLoader(ClassLoader classLoader) {
+        this.loader = classLoader;
     }
 }
