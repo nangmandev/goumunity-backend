0e60c9fe558382220ea04ad906085d8a782185de
Ryong <wkdcjdfyd@naver.com>
2024-01-17 13:09:27 +0900

    test: 회원가입, 이메일로 유저조회 test 작성

--- BE/src/main/java/com/ssafy/goumunity/user/dto/UserCreateDto.java
+++ BE/src/main/java/com/ssafy/goumunity/user/dto/UserCreateDto.java
@@ -1,10 +1,7 @@
 package com.ssafy.goumunity.user.dto;
 
 import com.ssafy.goumunity.user.domain.UserCategory;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -26,18 +23,20 @@ public class UserCreateDto {
 
     private Long monthBudget;
 
-    @Size(min = 1, max = 150, message = "나이는 1이상 150이하여야 합니다.")
+    @Min(message = "나이는 1살 이상이여야 합니다.", value = 0)
+    @Max(message = "나이는 150살 이하여야 합니다.", value = 150)
     private Integer age;
 
-    @NotBlank(message = "유저 카테고리는 필수값입니다.")
+    @NotNull(message = "유저 카테고리는 필수값입니다.")
     private UserCategory userCategory;
 
-    @Size(min = 0, max = 1, message = "성별은 0 또는 1 이여야합니다.")
-    @NotBlank(message = "성별은 필수값입니다.")
+    @Min(message = "성별은 0(남자) 또는 1(여자)로 입력해주세요.", value = 0)
+    @Max(message = "성별은 0(남자) 또는 1(여자)로 입력해주세요.", value = 1)
+    @NotNull(message = "성별은 필수값입니다.")
     private Integer gender;
 
     private String nickname;
 
-    @NotBlank(message = "지역 id는 필수값입니다.")
+    @NotNull(message = "지역 id는 필수값입니다.")
     private Integer regionId;
 }
