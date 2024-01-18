5ef76b3f7fa2f59061d6f681d29f6d5ac1b45ce3
김규현 <kgh2120@gmail.com>
2024-01-18 11:05:28 +0900

    chore: githooks 관련 내용 수정

--- BE/script/pre-commit.sh
+++ BE/script/pre-commit.sh
@@ -1,10 +1,16 @@
-!/bin/sh
+#!/bin/sh
+
+ls
 
 stagedFiles=$(git diff --staged --name-only)
 
 echo "stage 상태의 파일에 spotless를 적용해 포매팅을 합니다."
 
-./gradlew spotlessAppiy --daemon
+cd be
+
+./gradlew spotlessApply --daemon
+
+cd ..
 
 for file in $stagedFiles; do
   if test -f "$file"; then
