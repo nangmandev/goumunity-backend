4c976bfd2efaa7b6e7f59e637f8eecfe1ad00bc3
cbnu2017038040@gmail.com <wx47631!@#>
2024-01-24 21:37:27 +0900

    feat: 데이터 중복, 유효성 Exception 클래스 작성

---
+++ BE/src/main/java/com/ssafy/goumunity/common/exception/feed/RegionNotValidatedException.java
@@ -0,0 +1,7 @@
+package com.ssafy.goumunity.common.exception.feed;
+
+public class RegionNotValidatedException extends RuntimeException{
+    public RegionNotValidatedException(String message, Object T){
+        super("[RegionNotValidated] " + message + T.getClass());
+    }
+}
--- BE/src/main/java/com/ssafy/goumunity/common/exception/feed/ResourceNotFoundException.java
+++ BE/src/main/java/com/ssafy/goumunity/common/exception/feed/ResourceNotFoundException.java
@@ -3,6 +3,6 @@ package com.ssafy.goumunity.common.exception.feed;
 public class ResourceNotFoundException extends RuntimeException {
 
     public ResourceNotFoundException(String message, Object T) {
-        super(message + " " + T.getClass());
+        super("[ResourceNotFound] " + message + " " + T.getClass());
     }
 }
