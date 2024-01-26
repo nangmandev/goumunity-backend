1de712452fa374aca9b6674151c9011341f2ed50
cbnu2017038040@gmail.com <wx47631!@#>
2024-01-26 17:24:37 +0900

    feat: feed등록 dto 추가

--- BE/src/main/java/com/ssafy/goumunity/domain/feed/controller/request/FeedRegistRequest.java
+++ BE/src/main/java/com/ssafy/goumunity/domain/feed/controller/request/FeedRegistRequest.java
@@ -1,3 +1,19 @@
 package com.ssafy.goumunity.domain.feed.controller.request;
 
-public class FeedRegistRequest {}
+import com.ssafy.goumunity.domain.feed.domain.FeedCategory;
+import lombok.*;
+
+@Builder
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+public class FeedRegistRequest {
+    private String content;
+    private FeedCategory feedCategory;
+    private Integer price;
+    private Integer afterPrice;
+    private Integer profit;
+
+    private Long regionId;
+    private Long userId;
+}
