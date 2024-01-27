b3b8deef0f92bf0c0d6eda75fb1362011ecfe12c
cbnu2017038040@gmail.com <wx47631!@#>
2024-01-28 00:03:03 +0900

    feat: 피드좋아요, 좋아요개수확인 기능 작성

--- BE/src/main/java/com/ssafy/goumunity/domain/feed/infra/feedlike/FeedLikeJpaRepository.java
+++ BE/src/main/java/com/ssafy/goumunity/domain/feed/infra/feedlike/FeedLikeJpaRepository.java
@@ -1,3 +1,17 @@
 package com.ssafy.goumunity.domain.feed.infra.feedlike;
 
-public interface FeedLikeJpaRepository {}
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
+
+public interface FeedLikeJpaRepository extends JpaRepository<FeedLikeEntity, Long> {
+
+    @Query("select fl from FeedLikeEntity fl where fl.feedEntity.feedId=:feedId and fl.userEntity.id=:userId")
+    Optional<FeedLikeEntity> findOneByFeedIdAndUserId(@Param("feedId") Long feedId, @Param("userId") Long userId);
+
+    @Query("select count(fl) from FeedLikeEntity fl where fl.feedEntity.feedId=:feedId")
+    Integer countFeedLikeByFeedId(@Param("feedId") Long feedId);
+
+}
--- BE/src/main/java/com/ssafy/goumunity/domain/feed/service/FeedLikeServiceImpl.java
+++ BE/src/main/java/com/ssafy/goumunity/domain/feed/service/FeedLikeServiceImpl.java
@@ -1,3 +1,40 @@
 package com.ssafy.goumunity.domain.feed.service;
 
-public class FeedLikeServiceImpl {}
+import com.ssafy.goumunity.domain.feed.controller.request.FeedLikeCountRequest;
+import com.ssafy.goumunity.domain.feed.controller.request.FeedLikeRequest;
+import com.ssafy.goumunity.domain.feed.controller.response.FeedLikeCountResponse;
+import com.ssafy.goumunity.domain.feed.domain.FeedLike;
+import com.ssafy.goumunity.domain.feed.infra.feedlike.FeedLikeEntity;
+import com.ssafy.goumunity.domain.feed.service.post.FeedLikeRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class FeedLikeServiceImpl implements FeedLikeService{
+
+    private final FeedLikeRepository feedLikeRepository;
+
+    @Override
+    public boolean pushLikeButton(FeedLikeRequest feedLikeRequest, Long nowUserId) {
+
+        // TODO : user통합시 현재유저검증로직 추가
+
+        Optional<FeedLike> feedLike = feedLikeRepository.findOneByFeedIdAndUserId(feedLikeRequest.getFeedId(), feedLikeRequest.getUserId());
+
+        if(feedLike.isEmpty()){
+            feedLikeRepository.save(FeedLikeEntity.from(FeedLike.from(feedLikeRequest)));
+            return true;
+        } else {
+            feedLikeRepository.delete(FeedLikeEntity.from(feedLike.get()));
+            return false;
+        }
+    }
+
+    @Override
+    public FeedLikeCountResponse countFeedLikeByFeedId(FeedLikeCountRequest feedLikeCountRequest) {
+        return FeedLikeCountResponse.from(feedLikeRepository.countFeedLikeByFeedId(feedLikeCountRequest.getFeedId()));
+    }
+}
