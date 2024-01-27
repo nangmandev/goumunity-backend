f97657c0fcdc3a574fda5b2c5be8a7945f8fa6de
cbnu2017038040@gmail.com <wx47631!@#>
2024-01-27 16:13:19 +0900

    test: 지역 단건등록 validation테스트 추가

--- BE/src/test/java/com/ssafy/goumunity/domain/region/controller/RegionControllerTest.java
+++ BE/src/test/java/com/ssafy/goumunity/domain/region/controller/RegionControllerTest.java
@@ -2,14 +2,20 @@ package com.ssafy.goumunity.domain.region.controller;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ssafy.goumunity.config.SecurityConfig;
+import com.ssafy.goumunity.domain.region.controller.request.RegionRegistRequest;
 import com.ssafy.goumunity.domain.region.controller.response.RegionResponse;
+import com.ssafy.goumunity.domain.region.domain.Region;
 import com.ssafy.goumunity.domain.region.service.RegionService;
 import java.nio.charset.StandardCharsets;
 import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -43,14 +49,117 @@ class RegionControllerTest {
     @Autowired private MockMvc mockMvc;
 
     private ObjectMapper mapper;
+    private RegionRegistRequest regionRegistRequest;
 
     @BeforeEach
-    void 매퍼() {
+    void 매퍼등록및단건등록() throws Exception{
         mapper = new ObjectMapper();
+
+        regionRegistRequest = RegionRegistRequest.builder()
+                .si("서울시")
+                .gungu("중구")
+                .build();
+
+        RegionResponse regionResponse = RegionResponse.builder()
+                .regionId(Long.valueOf(1))
+                .si("서울시")
+                .gungu("중구")
+                .createdAt(Instant.now().toEpochMilli())
+                .updatedAt(Instant.now().toEpochMilli())
+                .build();
+
+        List<RegionResponse> regionResponses = new ArrayList<>();
+        regionResponses.add(regionResponse);
+
+        BDDMockito.given(
+                regionService.findOneByRegionId(Long.valueOf(1))
+        ).willReturn(regionResponse);
+
+        BDDMockito.given(regionService.findAll())
+                .willReturn(regionResponses);
+
+        mockMvc.perform(post("/api/regions").contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(regionRegistRequest))
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated());
+
     }
 
     @Nested
-    class 등록테스트 {}
+    class 등록테스트 {
+
+        @Test
+        @DisplayName("정상지역등록_성공_BeforeEach_이전")
+        @Deprecated
+        void 정상지역등록() throws Exception{
+
+            ResultActions resultActions = mockMvc
+                            .perform(get("/api/regions/1").contentType(MediaType.APPLICATION_JSON_UTF8))
+                            .andExpect(status().isOk());
+
+            RegionResponse result = mapper.readValue(
+                            resultActions.andReturn().getResponse().getContentAsString(StandardCharsets.UTF_8),
+                            RegionResponse.class);
+
+            SoftAssertions sa = new SoftAssertions();
+
+            sa.assertThat(result.getRegionId()).isEqualTo(Long.valueOf(1));
+            sa.assertThat(result.getSi()).isEqualTo("서울시");
+            sa.assertThat(result.getGungu()).isEqualTo("중구");
+
+            sa.assertAll();
+
+        }
+        
+        @Test
+        @DisplayName("널값등록_성공")
+        void NULL등록() throws Exception{
+
+            regionRegistRequest = RegionRegistRequest.builder()
+                    .si(null)
+                    .gungu(null)
+                    .build();
+
+            mockMvc.perform(post("/api/regions").contentType(MediaType.APPLICATION_JSON)
+                            .content(mapper.writeValueAsString(regionRegistRequest))
+                            .accept(MediaType.APPLICATION_JSON))
+                    .andExpect(status().isBadRequest());
+
+        }
+
+        @Test
+        @DisplayName("빈값등록_성공")
+        void 빈값등록() throws Exception{
+
+            regionRegistRequest = RegionRegistRequest.builder()
+                    .si("")
+                    .gungu("")
+                    .build();
+
+            mockMvc.perform(post("/api/regions").contentType(MediaType.APPLICATION_JSON)
+                            .content(mapper.writeValueAsString(regionRegistRequest))
+                            .accept(MediaType.APPLICATION_JSON))
+                    .andExpect(status().isBadRequest());
+
+        }
+
+        @Test
+        @DisplayName("공백등록_성공")
+        void 공백등록() throws Exception{
+
+            regionRegistRequest = RegionRegistRequest.builder()
+                    .si(" ")
+                    .gungu(" ")
+                    .build();
+
+            mockMvc.perform(post("/api/regions").contentType(MediaType.APPLICATION_JSON)
+                            .content(mapper.writeValueAsString(regionRegistRequest))
+                            .accept(MediaType.APPLICATION_JSON))
+                    .andExpect(status().isBadRequest());
+
+        }
+
+    }
 
     @Nested
     class 조회테스트 {
@@ -58,17 +167,6 @@ class RegionControllerTest {
         @Test
         @DisplayName("ID별_단건조회_성공")
         void 단건조회_ID별() throws Exception {
-
-            RegionResponse regionResponse =
-                    RegionResponse.builder()
-                            .regionId(Long.valueOf(1))
-                            .si("서울시")
-                            .gungu("중구")
-                            .createdAt(Instant.now().toEpochMilli())
-                            .updatedAt(Instant.now().toEpochMilli())
-                            .build();
-
-            BDDMockito.given(regionService.findOneByRegionId(any())).willReturn(regionResponse);
 
             ResultActions resultActions =
                     mockMvc
@@ -85,6 +183,26 @@ class RegionControllerTest {
             sa.assertThat(result.getRegionId()).isEqualTo(Long.valueOf(1));
             sa.assertThat(result.getSi()).isEqualTo("서울시");
             sa.assertThat(result.getGungu()).isEqualTo("중구");
+
+            sa.assertAll();
+        }
+
+        @Test
+        @DisplayName("전체조회_정상")
+        void 전체조회_정상() throws Exception{
+
+            ResultActions resultActions = mockMvc
+                            .perform(get("/api/regions").contentType(MediaType.APPLICATION_JSON_UTF8))
+                            .andExpect(status().isOk());
+
+            List<RegionResponse> result =
+                    mapper.readValue(
+                            resultActions.andReturn().getResponse().getContentAsString(StandardCharsets.UTF_8),
+                            List.class);
+
+            SoftAssertions sa = new SoftAssertions();
+
+            sa.assertThat(result.size()).isEqualTo(1);
 
             sa.assertAll();
         }
