e1e0b0af62657be9d0b2dbf094b64e9be55f7a01
cbnu2017038040@gmail.com <wx47631!@#>
2024-01-29 09:10:05 +0900

    test: feed 저장시 반환값 제거에 따른 테스트코드 작성

--- BE/src/test/java/com/ssafy/goumunity/domain/feed/service/FeedServiceTest.java
+++ BE/src/test/java/com/ssafy/goumunity/domain/feed/service/FeedServiceTest.java
@@ -1,63 +1,41 @@
 package com.ssafy.goumunity.domain.feed.service;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 import com.ssafy.goumunity.domain.feed.controller.request.FeedRegistRequest;
-import com.ssafy.goumunity.domain.feed.domain.Feed;
 import com.ssafy.goumunity.domain.feed.domain.FeedCategory;
-import com.ssafy.goumunity.domain.feed.infra.feed.FeedEntity;
 import com.ssafy.goumunity.domain.feed.service.post.FeedRepository;
-import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.BDDMockito;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
-
 @ExtendWith(MockitoExtension.class)
 class FeedServiceTest {
 
-    @Mock
-    FeedRepository feedRepository;
+    @Mock FeedRepository feedRepository;
 
-    @InjectMocks
-    FeedServiceImpl feedService;
+    @InjectMocks FeedServiceImpl feedService;
 
     @Nested
     class 입력테스트 {
         @Test
         void 정상입력테스트() {
 
-            FeedRegistRequest feedRegistRequest = FeedRegistRequest.builder()
-                    .feedCategory(FeedCategory.FUN)
-                    .content("hllo")
-                    .price(20000)
-                    .afterPrice(10000)
-                    .profit(10000)
-                    .userId(Long.valueOf(1))
-                    .regionId(Long.valueOf(1))
-                    .build();
+            FeedRegistRequest feedRegistRequest =
+                    FeedRegistRequest.builder()
+                            .feedCategory(FeedCategory.FUN)
+                            .content("hllo")
+                            .price(20000)
+                            .afterPrice(10000)
+                            .profit(10000)
+                            .userId(Long.valueOf(1))
+                            .regionId(Long.valueOf(1))
+                            .build();
 
-            Feed feed = Feed.from(feedRegistRequest);
-
-            BDDMockito.given(
-                    feedRepository.save(any())
-            ).willReturn(feed);
-
-            Feed result = feedService.save(feedRegistRequest);
-
-            SoftAssertions sa = new SoftAssertions();
-
-            sa.assertThat(result.getProfit()).isEqualTo(10000);
-            sa.assertThat(result.getPrice()).isEqualTo(20000);
-
-            sa.assertAll();
-
+            assertDoesNotThrow(() -> feedService.save(feedRegistRequest));
         }
-
     }
-
 }
