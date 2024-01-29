a44425c76fe41c4445cb4a620e13f0f716fdeaf4
장청룡 <wkdcjdfyd@naver.com>
2024-01-29 14:07:32 +0900

    test: 피드 좋아요, 좋아요 취소 테스트 작성

--- BE/src/main/java/com/ssafy/goumunity/domain/feed/domain/FeedLike.java
+++ BE/src/main/java/com/ssafy/goumunity/domain/feed/domain/FeedLike.java
@@ -1,6 +1,7 @@
 package com.ssafy.goumunity.domain.feed.domain;
 
 import java.time.Instant;
+import java.util.Objects;
 import lombok.*;
 
 @Builder
@@ -18,5 +19,22 @@ public class FeedLike {
 
     public static FeedLike from(Long userId, Long feedId) {
         return FeedLike.builder().userId(userId).feedId(feedId).build();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        FeedLike feedLike = (FeedLike) o;
+        return Objects.equals(feedLikeId, feedLike.feedLikeId)
+                && Objects.equals(feedId, feedLike.feedId)
+                && Objects.equals(userId, feedLike.userId)
+                && Objects.equals(createdAt, feedLike.createdAt)
+                && Objects.equals(updatedAt, feedLike.updatedAt);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(feedLikeId, feedId, userId, createdAt, updatedAt);
     }
 }
