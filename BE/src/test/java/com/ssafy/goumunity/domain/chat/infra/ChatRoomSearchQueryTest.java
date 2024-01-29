92b2c8feede4e812f5d5948fe5f11e7c8bafc80e
김규현 <kgh2120@gmail.com>
2024-01-29 10:20:26 +0900

    test: 거지방 해시태그 검색 태케 추가

--- BE/src/test/java/com/ssafy/goumunity/domain/chat/infra/ChatRoomSearchQueryTest.java
+++ BE/src/test/java/com/ssafy/goumunity/domain/chat/infra/ChatRoomSearchQueryTest.java
@@ -1,10 +1,10 @@
 package com.ssafy.goumunity.domain.chat.infra;
 
-import com.ssafy.goumunity.domain.chat.controller.response.ChatRoomSearchResponse;
+import static org.assertj.core.api.Assertions.assertThat;
+
 import com.ssafy.goumunity.domain.user.infra.UserEntity;
 import jakarta.persistence.EntityManager;
 import java.time.Instant;
-import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -21,7 +21,7 @@ class ChatRoomSearchQueryTest {
     @Autowired ChatRoomJpaRepository chatRoomJpaRepository;
 
     @Test
-    void jpaRepoTest() throws Exception {
+    void 거지방_검색_아이디로_검색() throws Exception {
         // given
         UserEntity users =
                 UserEntity.builder().nickname("1234").email("ssafy@gmail.com").password("1234").build();
@@ -61,12 +61,56 @@ class ChatRoomSearchQueryTest {
 
         Slice<ChatRoomEntity> slice =
                 chatRoomJpaRepository.searchChatRoom("거지방", Instant.now(), PageRequest.of(0, 10));
+        assertThat(slice.getContent().size()).isSameAs(10);
+        for (ChatRoomEntity chatRoomEntity : slice) {
+            System.out.println(chatRoomEntity);
+        }
+    }
 
-        List<ChatRoomSearchResponse> sut =
-                slice.getContent().stream().map(ChatRoomSearchResponse::from).toList();
+    @Test
+    void 거지방_검색_해시태그로_검색() throws Exception {
+        // given
+        UserEntity users =
+                UserEntity.builder().nickname("1234").email("ssafy@gmail.com").password("1234").build();
 
-        for (ChatRoomSearchResponse chatRoomSearchResponse : sut) {
-            System.out.println(chatRoomSearchResponse);
+        em.persist(users);
+
+        HashtagEntity h1 = HashtagEntity.builder().name("20대").build();
+        em.persist(h1);
+        HashtagEntity h2 = HashtagEntity.builder().name("관악구").build();
+        em.persist(h2);
+        HashtagEntity h3 = HashtagEntity.builder().name("10만원 미만").build();
+        em.persist(h3);
+
+        for (int i = 0; i < 10; i++) {
+            ChatRoomEntity chatRoom =
+                    ChatRoomEntity.builder()
+                            .title("거지방" + i)
+                            .capability(10)
+                            .host(users)
+                            .createdAt(Instant.ofEpochMilli(100L))
+                            .build();
+            em.persist(chatRoom);
+
+            em.persist(
+                    ChatRoomHashtagEntity.builder().chatRoom(chatRoom).hashtag(h1).sequence(1).build());
+
+            em.persist(
+                    ChatRoomHashtagEntity.builder().chatRoom(chatRoom).hashtag(h2).sequence(2).build());
+
+            em.persist(
+                    ChatRoomHashtagEntity.builder().chatRoom(chatRoom).hashtag(h3).sequence(3).build());
+        }
+
+        em.flush();
+        em.clear();
+        // when
+
+        Slice<ChatRoomEntity> slice =
+                chatRoomJpaRepository.searchChatRoom("20대", Instant.now(), PageRequest.of(0, 10));
+        assertThat(slice.getContent().size()).isSameAs(10);
+        for (ChatRoomEntity chatRoomEntity : slice) {
+            System.out.println(chatRoomEntity);
         }
     }
 }
