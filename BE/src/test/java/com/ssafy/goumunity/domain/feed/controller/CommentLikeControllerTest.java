bc1b7092f19a5bad48c3680c1a9af3b516ededba
장청룡 <wkdcjdfyd@naver.com>
2024-01-29 14:41:13 +0900

    refactor: 댓글 좋아요, 좋아요 취소 API 요청 URL 수정

--- BE/src/test/java/com/ssafy/goumunity/domain/feed/controller/CommentLikeControllerTest.java
+++ BE/src/test/java/com/ssafy/goumunity/domain/feed/controller/CommentLikeControllerTest.java
@@ -1,12 +1,15 @@
 package com.ssafy.goumunity.domain.feed.controller;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ssafy.goumunity.config.SecurityConfig;
 import com.ssafy.goumunity.domain.feed.service.CommentLikeService;
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityFilterAutoConfiguration;
@@ -27,11 +30,9 @@ import org.springframework.test.web.servlet.MockMvc;
         })
 class CommentLikeControllerTest {
 
-    @MockBean private CommentLikeService commentLikeService;
-
     @Autowired private MockMvc mockMvc;
 
-    private ObjectMapper mapper = new ObjectMapper();
+    @MockBean private CommentLikeService commentLikeService;
 
     @Nested
     @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -40,14 +41,14 @@ class CommentLikeControllerTest {
         @DisplayName("좋아요UP_성공")
         void 좋아요UP테스트() throws Exception {
 
-            mockMvc.perform(post("/api/comments/1/commentlikes")).andExpect(status().isCreated());
+            mockMvc.perform(post("/api/comments/1/like")).andExpect(status().isCreated());
         }
 
         @Test
         @DisplayName("좋아요DOWN_성공")
         void 좋아요DOWN테스트() throws Exception {
 
-            mockMvc.perform(delete("/api/comments/1/commentlikes")).andExpect(status().isOk());
+            mockMvc.perform(delete("/api/comments/1/unlike")).andExpect(status().isOk());
         }
     }
 }
