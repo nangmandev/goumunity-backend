96ccc213e4fd0f6f067a7901483088a55a5eaaa3
장청룡 <wkdcjdfyd@naver.com>
2024-01-30 12:57:48 +0900

    feat: 피드 조회(메인화면) API 구현

--- BE/src/main/java/com/ssafy/goumunity/domain/feed/controller/FeedController.java
+++ BE/src/main/java/com/ssafy/goumunity/domain/feed/controller/FeedController.java
@@ -1,9 +1,8 @@
 package com.ssafy.goumunity.domain.feed.controller;
 
+import com.ssafy.goumunity.common.util.SliceResponse;
 import com.ssafy.goumunity.domain.feed.controller.request.FeedRequest;
-import com.ssafy.goumunity.domain.feed.controller.response.FeedImgResponse;
 import com.ssafy.goumunity.domain.feed.controller.response.FeedResponse;
-import com.ssafy.goumunity.domain.feed.service.FeedImgService;
 import com.ssafy.goumunity.domain.feed.service.FeedService;
 import com.ssafy.goumunity.domain.user.domain.User;
 import jakarta.annotation.Nullable;
@@ -11,6 +10,8 @@ import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
@@ -23,17 +24,6 @@ import org.springframework.web.multipart.MultipartFile;
 public class FeedController {
 
     private final FeedService feedService;
-    private final FeedImgService feedImgService;
-
-    @GetMapping("/{feedId}")
-    public ResponseEntity<FeedResponse> findOneByFeedId(@PathVariable Long feedId) {
-        return ResponseEntity.ok(feedService.findOneByFeedId(feedId));
-    }
-
-    @GetMapping("/{feedId}/feedimgs")
-    public ResponseEntity<List<FeedImgResponse>> findAllByFeedId(@PathVariable Long feedId) {
-        return ResponseEntity.ok(feedImgService.findAllByFeedId(feedId));
-    }
 
     @PostMapping
     public ResponseEntity<Void> createFeed(
@@ -42,6 +32,13 @@ public class FeedController {
             @RequestPart("images") @Nullable List<MultipartFile> images) {
         feedService.createFeed(user.getId(), feedRequest, images);
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<SliceResponse<FeedResponse>> find(
+            @RequestParam("time") Long time, Pageable pageable) {
+        Slice<FeedResponse> feeds = feedService.find(time, pageable);
+        return ResponseEntity.ok(SliceResponse.from(feeds.getContent(), feeds.hasNext()));
     }
 
     @DeleteMapping("/{feedId}")
---
+++ BE/src/main/java/com/ssafy/goumunity/domain/feed/infra/feed/FeedQueryDslRepository.java
@@ -0,0 +1,55 @@
+package com.ssafy.goumunity.domain.feed.infra.feed;
+
+import static com.ssafy.goumunity.domain.feed.infra.comment.QCommentEntity.commentEntity;
+import static com.ssafy.goumunity.domain.feed.infra.feed.QFeedEntity.feedEntity;
+import static com.ssafy.goumunity.domain.feed.infra.feedimg.QFeedImgEntity.feedImgEntity;
+import static com.ssafy.goumunity.domain.feed.infra.feedlike.QFeedLikeEntity.feedLikeEntity;
+import static com.ssafy.goumunity.domain.region.infra.QRegionEntity.regionEntity;
+import static com.ssafy.goumunity.domain.user.infra.QUserEntity.userEntity;
+
+import com.querydsl.jpa.JPAExpressions;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.ssafy.goumunity.common.util.SliceUtils;
+import com.ssafy.goumunity.domain.feed.controller.response.FeedResponse;
+import com.ssafy.goumunity.domain.feed.controller.response.QFeedResponse;
+import java.time.Instant;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.SliceImpl;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class FeedQueryDslRepository {
+
+    private final JPAQueryFactory queryFactory;
+
+    public Slice<FeedResponse> find(Instant time, Pageable pageable) {
+        final List<FeedResponse> res =
+                queryFactory
+                        .query()
+                        .select(
+                                new QFeedResponse(
+                                        feedEntity,
+                                        JPAExpressions.select(commentEntity.count())
+                                                .from(commentEntity)
+                                                .where(feedEntity.eq(commentEntity.feedEntity)),
+                                        JPAExpressions.select(feedLikeEntity.count())
+                                                .from(feedLikeEntity)
+                                                .where(feedEntity.eq(feedLikeEntity.feedEntity))))
+                        .from(feedEntity)
+                        .leftJoin(feedEntity.images, feedImgEntity)
+                        .leftJoin(feedEntity.userEntity, userEntity)
+                        .leftJoin(feedEntity.regionEntity, regionEntity)
+                        .groupBy(feedEntity)
+                        .having(feedEntity.createdAt.before(time))
+                        .orderBy(feedEntity.createdAt.desc())
+                        .offset(pageable.getOffset())
+                        .limit(pageable.getPageSize() + 1)
+                        .fetch();
+
+        return new SliceImpl<>(res, pageable, SliceUtils.hasNext(res, pageable));
+    }
+}
