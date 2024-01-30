9067aaa84aa71449c8dd8d9c458880412bb5beb0
장청룡 <wkdcjdfyd@naver.com>
2024-01-30 14:03:52 +0900

    fix: 답글 좋아요 기능 버그 수정

--- BE/src/main/java/com/ssafy/goumunity/domain/feed/service/post/ReplyLikeRepository.java
+++ BE/src/main/java/com/ssafy/goumunity/domain/feed/service/post/ReplyLikeRepository.java
@@ -1,10 +1,13 @@
 package com.ssafy.goumunity.domain.feed.service.post;
 
 import com.ssafy.goumunity.domain.feed.domain.ReplyLike;
+import java.util.Optional;
 
 public interface ReplyLikeRepository {
 
     void createReplyLike(ReplyLike replyLike);
+
+    Optional<ReplyLike> findOneByUserIdAndReplyId(Long userId, Long replyId);
 
     void deleteReplyLike(ReplyLike replyLike);
 
