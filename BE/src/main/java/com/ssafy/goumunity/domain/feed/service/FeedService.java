9e2be77b4ad1c0bcb9233627ee66a488e1258286
cbnu2017038040@gmail.com <wx47631!@#>
2024-02-01 00:38:58 +0900

    feat: 같은 지역 데이터를 받아와야 하기 때문에 지역id값 추가 리턴타입 추천지역리스트를 래핑하는 DTO로 변경

--- BE/src/main/java/com/ssafy/goumunity/domain/feed/service/FeedService.java
+++ BE/src/main/java/com/ssafy/goumunity/domain/feed/service/FeedService.java
@@ -1,8 +1,13 @@
 package com.ssafy.goumunity.domain.feed.service;
 
 import com.ssafy.goumunity.domain.feed.controller.request.FeedRequest;
+import com.ssafy.goumunity.domain.feed.controller.response.FeedRecommendResponse;
 import com.ssafy.goumunity.domain.feed.controller.response.FeedResponse;
 import java.util.List;
+
+import com.ssafy.goumunity.domain.feed.domain.FeedRecommendResource;
+import com.ssafy.goumunity.domain.feed.domain.FeedWeight;
+import com.ssafy.goumunity.domain.user.domain.User;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.web.multipart.MultipartFile;
@@ -11,7 +16,7 @@ public interface FeedService {
 
     void createFeed(Long userId, FeedRequest.Create feedRequest, List<MultipartFile> images);
 
-    Slice<FeedResponse> findFeed(Long time, Pageable pageable);
+    FeedRecommendResponse findFeed(User user, Long time, Long regionId);
 
     FeedResponse findOneByFeedId(Long feedId);
 
