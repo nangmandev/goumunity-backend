40b80802a28b2950a59dd28a6e7b8808895cbbde
cbnu2017038040@gmail.com <wx47631!@#>
2024-02-01 00:37:53 +0900

    feat: 전체 피드 데이터를 한번에 넘기도록 수정

--- BE/src/main/java/com/ssafy/goumunity/domain/feed/service/post/FeedRepository.java
+++ BE/src/main/java/com/ssafy/goumunity/domain/feed/service/post/FeedRepository.java
@@ -3,7 +3,11 @@ package com.ssafy.goumunity.domain.feed.service.post;
 import com.ssafy.goumunity.domain.feed.controller.response.FeedResponse;
 import com.ssafy.goumunity.domain.feed.domain.Feed;
 import java.time.Instant;
+import java.util.List;
 import java.util.Optional;
+
+import com.ssafy.goumunity.domain.feed.domain.FeedRecommendResource;
+import com.ssafy.goumunity.domain.region.infra.QRegionEntity;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 
@@ -11,7 +15,7 @@ public interface FeedRepository {
 
     Feed save(Feed feed);
 
-    Slice<FeedResponse> findFeed(Instant time, Pageable pageable);
+    List<FeedRecommendResource> findFeed(Long userId, Instant time, Long regionId);
 
     FeedResponse findOneFeed(Long feedId);
 
