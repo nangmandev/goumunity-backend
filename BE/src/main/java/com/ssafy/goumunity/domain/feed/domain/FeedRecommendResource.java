210b58c688bcc105372a85391bb45f082913d90d
cbnu2017038040@gmail.com <wx47631!@#>
2024-02-01 00:36:42 +0900

    feat: 프론트에서 원하는 데이터들 가지고 있는 dto 작성

---
+++ BE/src/main/java/com/ssafy/goumunity/domain/feed/domain/FeedRecommendResource.java
@@ -0,0 +1,51 @@
+package com.ssafy.goumunity.domain.feed.domain;
+
+import com.querydsl.core.annotations.QueryProjection;
+import com.ssafy.goumunity.domain.feed.controller.response.FeedImgResponse;
+import com.ssafy.goumunity.domain.feed.infra.feed.FeedEntity;
+import com.ssafy.goumunity.domain.region.controller.response.RegionResponse;
+import com.ssafy.goumunity.domain.user.dto.UserResponse;
+import lombok.*;
+
+import java.util.List;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+@Getter
+public class FeedRecommendResource {
+    private Long feedId;
+    private String content;
+    private FeedCategory feedCategory;
+    private Integer price;
+    private Integer afterPrice;
+
+    private RegionResponse region;
+    private UserResponse user;
+
+    private Long createdAt;
+    private Long updatedAt;
+
+    private List<FeedImgResponse> images;
+    private Long commentCount;
+    private Long likeCount;
+
+    private boolean iLikeThat;
+
+    @QueryProjection
+    public FeedRecommendResource(FeedEntity feed, Long commentCount, Long likeCount, boolean iLikeThat) {
+        this.feedId = feed.getFeedId();
+        this.content = feed.getContent();
+        this.feedCategory = feed.getFeedCategory();
+        this.price = feed.getPrice();
+        this.afterPrice = feed.getAfterPrice();
+        this.region = RegionResponse.from(feed.getRegionEntity().to());
+        this.user = UserResponse.from(feed.getUserEntity().toModel());
+        this.createdAt = feed.getCreatedAt().toEpochMilli();
+        this.updatedAt = feed.getUpdatedAt().toEpochMilli();
+        this.images = feed.getImages().stream().map(FeedImgResponse::from).toList();
+        this.commentCount = commentCount;
+        this.likeCount = likeCount;
+        this.iLikeThat = iLikeThat;
+    }
+}
