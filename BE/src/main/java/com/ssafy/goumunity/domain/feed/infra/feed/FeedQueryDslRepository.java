1c459d729acb2dbb675ba3624bf928c790e46e74
cbnu2017038040@gmail.com <wx47631!@#>
2024-02-01 09:57:53 +0900

    fix: 쿼리 수정

--- BE/src/main/java/com/ssafy/goumunity/domain/feed/infra/feed/FeedQueryDslRepository.java
+++ BE/src/main/java/com/ssafy/goumunity/domain/feed/infra/feed/FeedQueryDslRepository.java
@@ -9,20 +9,13 @@ import static com.ssafy.goumunity.domain.user.infra.QUserEntity.userEntity;
 
 import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
-import com.ssafy.goumunity.common.util.SliceUtils;
 import com.ssafy.goumunity.domain.feed.controller.response.FeedResponse;
 import com.ssafy.goumunity.domain.feed.controller.response.QFeedResponse;
-import java.time.Instant;
-import java.util.List;
-
 import com.ssafy.goumunity.domain.feed.domain.FeedRecommendResource;
 import com.ssafy.goumunity.domain.feed.domain.QFeedRecommendResource;
-import com.ssafy.goumunity.domain.region.infra.QRegionEntity;
-import com.ssafy.goumunity.domain.region.infra.RegionEntity;
+import java.time.Instant;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Slice;
-import org.springframework.data.domain.SliceImpl;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -33,29 +26,29 @@ public class FeedQueryDslRepository {
 
     public List<FeedRecommendResource> findFeed(Long userId, Instant time, Long regionId) {
         return queryFactory
-                        .query()
-                        .select(
-                                new QFeedRecommendResource(
-                                        feedEntity,
-                                        JPAExpressions.select(commentEntity.count())
-                                                .from(commentEntity)
-                                                .where(feedEntity.eq(commentEntity.feedEntity)),
-                                        JPAExpressions.select(feedLikeEntity.count())
-                                                .from(feedLikeEntity)
-                                                .where(feedEntity.eq(feedLikeEntity.feedEntity)),
-                                        JPAExpressions.selectFrom(feedLikeEntity)
-                                                .where(feedLikeEntity.userEntity.id.eq(userId))
-                                                .where(feedLikeEntity.feedEntity.feedId.eq(feedEntity.feedId))
-                                                .exists()
-                                        ))
-                        .from(feedEntity)
-                        .leftJoin(feedEntity.images, feedImgEntity)
-                        .leftJoin(feedEntity.userEntity, userEntity)
-                        .leftJoin(feedEntity.regionEntity, regionEntity).on(regionEntity.regionId.eq(regionId))
-                        .groupBy(feedEntity)
-                        .having(feedEntity.createdAt.before(time))
-                        .orderBy(feedEntity.createdAt.desc(), feedImgEntity.sequence.asc())
-                        .fetch();
+                .query()
+                .select(
+                        new QFeedRecommendResource(
+                                feedEntity,
+                                JPAExpressions.select(commentEntity.count())
+                                        .from(commentEntity)
+                                        .where(feedEntity.eq(commentEntity.feedEntity)),
+                                JPAExpressions.select(feedLikeEntity.count())
+                                        .from(feedLikeEntity)
+                                        .where(feedEntity.eq(feedLikeEntity.feedEntity)),
+                                JPAExpressions.selectFrom(feedLikeEntity)
+                                        .where(feedLikeEntity.userEntity.id.eq(userId))
+                                        .where(feedLikeEntity.feedEntity.eq(feedEntity))
+                                        .exists()))
+                .from(feedEntity)
+                .leftJoin(feedEntity.images, feedImgEntity)
+                .leftJoin(feedEntity.userEntity, userEntity)
+                .leftJoin(feedEntity.regionEntity, regionEntity)
+                .where(feedEntity.regionEntity.regionId.eq(regionId))
+                .groupBy(feedEntity)
+                .having(feedEntity.createdAt.before(time))
+                .orderBy(feedEntity.createdAt.desc(), feedImgEntity.sequence.asc())
+                .fetch();
     }
 
     public FeedResponse findOneFeed(Long feedId) {
