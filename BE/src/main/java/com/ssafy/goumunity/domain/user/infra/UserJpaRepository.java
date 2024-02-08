38637ef72bcafe3d5cd0effae7c73b4dc5617392
장청룡 <wkdcjdfyd@naver.com>
2024-02-08 13:25:00 +0900

    refactor: 유저 response 변경

--- BE/src/main/java/com/ssafy/goumunity/domain/user/infra/UserJpaRepository.java
+++ BE/src/main/java/com/ssafy/goumunity/domain/user/infra/UserJpaRepository.java
@@ -3,8 +3,12 @@ package com.ssafy.goumunity.domain.user.infra;
 import com.ssafy.goumunity.domain.user.domain.UserStatus;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface UserJpaRepository extends JpaRepository<UserEntity, Long> {
+
+    @Query(
+            "select u from UserEntity u join fetch u.regionEntity where u.email = :email and u.userStatus = :userStatus")
     Optional<UserEntity> findByEmailAndUserStatus(String email, UserStatus userStatus);
 
     boolean existsByNickname(String nickname);
