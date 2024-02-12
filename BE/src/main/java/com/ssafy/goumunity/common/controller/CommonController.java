c34c6759b638bab9219c12019a978d0bb1f12c3d
김규현 <kgh2120@gmail.com>
2024-02-13 01:47:20 +0900

    fix: CommonController에 @RestController 어노테이션 추가

--- BE/src/main/java/com/ssafy/goumunity/common/controller/CommonController.java
+++ BE/src/main/java/com/ssafy/goumunity/common/controller/CommonController.java
@@ -3,14 +3,18 @@ package com.ssafy.goumunity.common.controller;
 import com.ssafy.goumunity.common.controller.response.ImageUploadResponse;
 import com.ssafy.goumunity.common.service.FileUploadService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+@Slf4j
 @RequiredArgsConstructor
-@RequestMapping("/common")
+@RequestMapping("/api/common")
+@RestController
 public class CommonController {
     private final FileUploadService fileUploadService;
 
