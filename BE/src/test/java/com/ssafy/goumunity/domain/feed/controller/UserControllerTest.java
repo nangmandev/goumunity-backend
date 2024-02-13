05bc8306dac754d39e987ea166e618e322f89c5d
한규준 <>
2024-02-13 14:12:35 +0900

    test: 콘솔 출력 코드, 세미콜론 하나더 들어간부분 수정

--- BE/src/test/java/com/ssafy/goumunity/domain/feed/controller/UserControllerTest.java
+++ BE/src/test/java/com/ssafy/goumunity/domain/feed/controller/UserControllerTest.java
@@ -4,7 +4,6 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ssafy.goumunity.common.config.SecurityConfig;
 import com.ssafy.goumunity.domain.feed.controller.response.FeedSearchResult;
 import com.ssafy.goumunity.domain.feed.controller.response.SavingResult;
@@ -29,7 +28,6 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
 
 @WebMvcTest(
         controllers = {UserController.class},
@@ -50,12 +48,8 @@ class UserControllerTest {
 
     @Autowired MockMvc mockMvc;
 
-    private ObjectMapper mapper;
-
     @Test
     void 유저별_피드조회테스트() throws Exception {
-
-        mapper = new ObjectMapper();
 
         List<FeedSearchResource> resources = new ArrayList<>();
         for (int i = 0; i < 10; i++) {
@@ -67,16 +61,11 @@ class UserControllerTest {
 
         BDDMockito.given(feedService.findAllFeedByUserId(any())).willReturn(result);
 
-        ResultActions resultActions =
-                mockMvc.perform(get("/api/users/1/feeds")).andExpect(status().isOk());
-
-        System.out.println(resultActions.andReturn().getResponse().getContentAsString());
+        mockMvc.perform(get("/api/users/1/feeds")).andExpect(status().isOk());
     }
 
     @Test
     void 유저별_절약내역테스트() throws Exception {
-
-        mapper = new ObjectMapper();
 
         List<SavingResource> resources = new ArrayList<>();
 
@@ -95,9 +84,6 @@ class UserControllerTest {
 
         BDDMockito.given(feedService.findAllSavingByUserId(any())).willReturn(result);
 
-        ResultActions resultActions =
-                mockMvc.perform(get("/api/users/1/savings")).andExpect(status().isOk());
-
-        System.out.println(resultActions.andReturn().getResponse().getContentAsString());
+        mockMvc.perform(get("/api/users/1/savings")).andExpect(status().isOk());
     }
 }
